a1c7f4319303c44c8c32c430ed141b22baf43c39
chrisgubish <chris.p.gubish@gmail.com>
2025-11-29 10:08:24 -0800

    Fix typo in pool size

--- kai_core/backend/models/database.py
+++ kai_core/backend/models/database.py
@@ -17,7 +17,7 @@ if not DATABASE_URL:
 engine = create_engine(
     DATABASE_URL, 
     poolclass=pool.QueuePool,
-    pool_sizer=10,
+    pool_size=10,
     max_overflow=20,
     pool_timeout=30,
     pool_recycle=3600,
